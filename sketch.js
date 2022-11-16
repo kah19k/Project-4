81237b4f683cc862cf83380ef5a0baa608faaae9
Kelly Hammer <kah19k@fsu.edu>
2022-11-16 11:01:55 -0500

    Added in my model

---
+++ sketch.js
@@ -0,0 +1,21 @@
+
+let teapot;
+
+function preload() {
+  // Load model with normalise parameter set to true
+  teapot = loadModel('assets/torsofemale.obj', true);
+}
+
+function setup() {
+  createCanvas(100, 100, WEBGL);
+  describe('Vertically rotating 3-d teapot with red, green and blue gradient.');
+}
+
+function draw() {
+  background(200);
+  scale(0.4); // Scaled to make model fit into canvas
+  rotateX(frameCount * 0.01);
+  rotateY(frameCount * 0.01);
+  normalMaterial(); // For effect
+  model(teapot);
+}
