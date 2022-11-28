39462121fee15e50dae855c8eceb1bb9529e503c
Kelly Hammer <kah19k@fsu.edu>
2022-11-28 14:25:48 -0500

    Getting the words placed.

--- sketch.js
+++ sketch.js
@@ -1,25 +1,45 @@
 
-let torsofemale;
+let newtorsofemale;
 
 function preload() {
   // Load model with normalise parameter set to true
-  torsofemale = loadModel('assets/torsofemale.obj', true);
+  newtorsofemale = loadModel('assets/newtorsofemale.obj', true);
 }
 
 function setup() {
   createCanvas(1200, 700, WEBGL);
-  
+  textScreen = createGraphics(1200,700)
+  textScreen.text("NO",600,300)
+  textScreen
+  textSize(300);
+  stroke(0);
 }
 
 function draw() {
   background(300);
   scale(3); // Scaled to make model fit into canvas
   let jitter = 0;
-  if (mouseIsPressed) {  jitter = random(-.1,.1); }
+  if (mouseIsPressed) { 
+   jitter = random(-.1,.1);
+   image(textScreen, -width/2, -height/2)
+   image(textScreen, -width/2.25, -height/2.25)
+   image(textScreen, -width/1.75, -height/2.25)
+   image(textScreen, -width/2, -height/2.75)
+   image(textScreen, -width/2.50, -height/1.75)
+   image(textScreen, -width/2.50, -height/3.25)
+   image(textScreen, -width/1.70, -height/3.25)
+   image(textScreen, -width/1.70, -height/1.75)
+  c
+   drawingContext.disable(drawingContext.DEPTH_TEST)
+   //drawingContext.enable(drawingContext.BLEND)
+   //drawingContext.enable(drawingContext.DEPTH_TEST)
+   
+  }
+  
   rotateX(frameCount * 0.01 + jitter);
   rotateY(frameCount * 0.01 + jitter);
   normalMaterial(); // For effect
-  model(torsofemale);
+  model(newtorsofemale);
   orbitControl();
-
+  
 }
