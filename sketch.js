890ce8b32f9abf5f919ca2b04193753d266ec4ab
Your Name <you@example.com>
2022-11-29 23:17:03 -0500

    Finalizing Project

--- sketch.js
+++ sketch.js
@@ -1,5 +1,6 @@
 
 let newtorsofemale;
+var clicks = 0;
 
 function preload() {
   // Load model with normalise parameter set to true
@@ -7,33 +8,73 @@ function preload() {
 }
 
 function setup() {
-  createCanvas(1200, 700, WEBGL);
-  textScreen = createGraphics(1200,700)
-  textScreen.text("NO",600,300)
+  createCanvas(2400, 1200, WEBGL);
+  textScreen = createGraphics(2400,1200)
+  textScreen.text("STOP!!!",1200, 500)
   textScreen
-  textSize(300);
-  stroke(0);
+  fill(0);
+textSize(12);
+textFont('Georgia');
+  textSize(800);
+  stroke(10);
+  let lights = [
+  { c: '#f00', t: 1.12, p: 1.91, r: 0.2 },
+  { c: '#0f0', t: 1.21, p: 1.31, r: 0.2 },
+  { c: '#00f', t: 1.37, p: 1.57, r: 0.2 },
+  { c: '#ff0', t: 1.12, p: 1.91, r: 0.7 },
+  { c: '#0ff', t: 1.21, p: 1.31, r: 0.7 },
+  { c: '#f0f', t: 1.37, p: 1.57, r: 0.7 }
+];
 }
 
 function draw() {
-  background(300);
+  background(600);
+  textSize(800);
+  stroke(10);
   scale(3); // Scaled to make model fit into canvas
   let jitter = 0;
   if (mouseIsPressed) { 
    jitter = random(-.1,.1);
-   image(textScreen, -width/2, -height/2)
-   image(textScreen, -width/2.25, -height/2.25)
-   image(textScreen, -width/1.75, -height/2.25)
-   image(textScreen, -width/2, -height/2.75)
-   image(textScreen, -width/2.50, -height/1.75)
-   image(textScreen, -width/2.50, -height/3.25)
-   image(textScreen, -width/1.70, -height/3.25)
-   image(textScreen, -width/1.70, -height/1.75)
-  c
-   drawingContext.disable(drawingContext.DEPTH_TEST)
-   //drawingContext.enable(drawingContext.BLEND)
+   background('rgb(100%,0%,10%)')
+
+   directionalLight(color('#222'), 1, 1, 1);
+   image(textScreen, -width/2, -height/2);
+   image(textScreen, -width/2.15, -height/2);
+   image(textScreen, -width/2.25, -height/2.25);
+   image(textScreen, -width/2.35, -height/2.25);
+   image(textScreen, -width/1.75, -height/2.25);
+   image(textScreen, -width/1.85, -height/2.25);
+   image(textScreen, -width/2, -height/2.75) ;
+   image(textScreen, -width/2, -height/2.85);
+   image(textScreen, -width/2.50, -height/1.75) ;
+   image(textScreen, -width/2.50, -height/1.85);
+   image(textScreen, -width/2.50, -height/3.25);
+   image(textScreen, -width/2.50, -height/3);
+   image(textScreen, -width/1.70, -height/3.25);
+   image(textScreen, -width/1.70, -height/2.75);
+   image(textScreen, -width/1.70, -height/1.75);
+
+   image(textScreen, -width/1.23, -height/1.95);
+   image(textScreen, -width/2.0, -height/2.15);
+   image(textScreen, -width/2.35, -height/2.15);
+   image(textScreen, -width/2.45, -height/2.15);
+   image(textScreen, -width/1.85, -height/2.15);
+   image(textScreen, -width/1.95, -height/2.15);
+   image(textScreen, -width/2.10, -height/2.75) ;
+   image(textScreen, -width/2, -height/2.85);
+   image(textScreen, -width/1.50, -height/1.65) ;
+   image(textScreen, -width/1.50, -height/1.35);
+   image(textScreen, -width/1.50, -height/3.15);
+   image(textScreen, -width/1.50, -height/3.10);
+   image(textScreen, -width/1.75, -height/3.15);
+   image(textScreen, -width/1.75, -height/2.65);
+   image(textScreen, -width/1.75, -height/1.65);
+
+   drawingContext.disable(drawingContext.DEPTH_TEST);
+   drawingContext.enable(drawingContext.BLEND);
    //drawingContext.enable(drawingContext.DEPTH_TEST)
    
+  
   }
   
   rotateX(frameCount * 0.01 + jitter);
@@ -41,5 +82,8 @@ function draw() {
   normalMaterial(); // For effect
   model(newtorsofemale);
   orbitControl();
+  
+  
+  
   
 }
